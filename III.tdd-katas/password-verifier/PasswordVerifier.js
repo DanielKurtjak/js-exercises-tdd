c78e85379e8def459c79c24583355d240a136c20
Daniel Kurtjak <danielkurtjak@gmail.com>
2020-09-07 22:51:18 +0200

    Password verified with deleyed response tests

--- III.tdd-katas/password-verifier/PasswordVerifier.js
+++ III.tdd-katas/password-verifier/PasswordVerifier.js
@@ -1,11 +1,66 @@
+const hasLowerCaseLetter = (word) => /[a-z]/.test(word);
+const hasUpperCaseLetter = (word) => /[A-Z]/.test(word);
+const hasDigit = (word) => /\d/.test(word);
+const isNotNull = (word) => word !== null;
+const isLongEnough = (word) => word.length > 8;
+
+const isNotNullDelayed = (word) =>
+  new Promise((resolve) => setTimeout(() => resolve(isNotNull(word)), 1000));
+
+const isLongEnoughDelayed = (word) =>
+  new Promise((resolve) => setTimeout(() => resolve(isLongEnough(word)), 1000));
+
+const hasLowerCaseLetterDelayed = (word) =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve(hasLowerCaseLetter(word)), 1000)
+  );
+
+const hasUpperCaseLetterDelayed = (word) =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve(hasUpperCaseLetter(word)), 1000)
+  );
+
+const hasDigitDelayed = (word) =>
+  new Promise((resolve) => setTimeout(() => resolve(hasDigit(word)), 1000));
+
 class PasswordVerifier {
   Verify(password) {
-    if (password === null) return false;
-    if (password.length <= 8) return false;
-    if (password.toLowerCase() === password) return false;
-    if (password.toUpperCase() === password) return false;
-    if (!/\d/.test(password)) return false;
+    if (!isNotNull(password)) return false;
+    if (!isLongEnough(password)) return false;
+    if (!hasLowerCaseLetter(password)) return false;
+    if (!hasUpperCaseLetter(password)) return false;
+    if (!hasDigit(password)) return false;
     return true;
+  }
+
+  async VerifyAdvance(password) {
+    const mandatoryCondition = await hasLowerCaseLetterDelayed(password);
+    if (!mandatoryCondition) return false;
+
+    const condition1 = await isNotNullDelayed(password);
+    const condition2 = await isLongEnoughDelayed(password);
+
+    if (condition1 && condition2) return true; //Third mandatory already passed
+    const condition3 = await hasUpperCaseLetterDelayed(password);
+    const firstThree = condition1 + condition2 + condition3;
+    if (firstThree === 2) return true; //Third mandatory already passed
+    if (firstThree === 0) return false; //With last one there will only be two passing (last one and mandatory one)
+    const condition5 = await hasDigitDelayed(password);
+    return condition5; //This condition is a decider
+
+    //One implemetation with speed increased
+    // const mandatoryCondition = await hasUpperCaseLetter(password);
+    // if (!mandatoryCondition) return false;
+
+    // const coditions = [
+    //   password !== null,
+    //   password.length > 8,
+    //   hasLowerCaseLetter(password),
+    //   mandatoryCondition,
+    //   hasDigit(password),
+    // ];
+
+    // return coditions.reduce((a, i) => a + i, 0) >= 3;
   }
 }
 
--- III.tdd-katas/password-verifier/PasswordVerifier.test.js
+++ III.tdd-katas/password-verifier/PasswordVerifier.test.js
@@ -1,60 +1,107 @@
 const PasswordVerifier = require("./PasswordVerifier.js");
 
 describe("PasswordVerifier", () => {
-  let passwordVerifier;
-  beforeEach(() => {
-    passwordVerifier = new PasswordVerifier();
+  describe("with regular Verify", () => {
+    let passwordVerifier;
+    beforeEach(() => {
+      passwordVerifier = new PasswordVerifier();
+    });
+
+    test("password should be larger than 8 chars", () => {
+      const result = passwordVerifier.Verify("VeryLongPassword1");
+
+      expect(result).toBe(true);
+
+      const resutlShouldBeFalseFor8CharLongPass = passwordVerifier.Verify(
+        "Exact8ch"
+      );
+
+      expect(resutlShouldBeFalseFor8CharLongPass).toBe(false);
+
+      const result2 = passwordVerifier.Verify("Short1");
+
+      expect(result2).toBe(false);
+    });
+
+    test("password should not be null", () => {
+      const result = passwordVerifier.Verify(null);
+
+      expect(result).toBe(false);
+    });
+
+    test("password should have letters", () => {
+      const failsForLongButAllNumbers = passwordVerifier.Verify("123456576788");
+
+      expect(failsForLongButAllNumbers).toBe(false);
+    });
+
+    test("password should have one uppercase letter at least", () => {
+      const result = passwordVerifier.Verify("VeryLongPassword1");
+
+      expect(result).toBe(true);
+
+      const result2 = passwordVerifier.Verify("verylongpasswordallsmallfails1");
+
+      expect(result2).toBe(false);
+    });
+
+    test("password should have one lowercase letter at least", () => {
+      const result = passwordVerifier.Verify("VeryLongPassword1");
+
+      expect(result).toBe(true);
+
+      const result2 = passwordVerifier.Verify("VERY_LONG_ALL_UPPER_FAILS1");
+
+      expect(result2).toBe(false);
+    });
+
+    test("password should have one number at least", () => {
+      const result = passwordVerifier.Verify("VeryLongPassword1");
+
+      expect(result).toBe(true);
+
+      const result2 = passwordVerifier.Verify("VeryLongPassword");
+
+      expect(result2).toBe(false);
+    });
   });
 
-  test("password should be larger than 8 chars", () => {
-    const result = passwordVerifier.Verify("VeryLongPassword1");
+  describe("with regular advanced, more simple verify", () => {
+    let passwordVerifier;
 
-    expect(result).toBe(true);
+    beforeEach(() => {
+      passwordVerifier = new PasswordVerifier();
+    });
 
-    const resutlShouldBeFalseFor8CharLongPass = passwordVerifier.Verify(
-      "Exact8ch"
-    );
+    test("password should be false if there are no lowercase letters", async () => {
+      const result = await passwordVerifier.VerifyAdvance("VP1125415");
 
-    expect(resutlShouldBeFalseFor8CharLongPass).toBe(false);
+      expect(result).toBe(false);
+    }, 1040);
 
-    const result2 = passwordVerifier.Verify("Short1");
+    test("password not should pass if at least three conditions are met with mandatory one", async () => {
+      const twoLowerCaseAndNotNull = "vvv";
+      const result = await passwordVerifier.VerifyAdvance(
+        twoLowerCaseAndNotNull
+      );
 
-    expect(result2).toBe(false);
-  });
+      expect(result).toBe(false);
+    }, 5500);
 
-  test("password should not be null", () => {
-    const result = passwordVerifier.Verify(null);
+    test("password should pass in less then 4000ms if first three conditions are met", async () => {
+      const notNullLongEnoughAndHasLowerCaseLetter = "abcdefghi";
+      const result = await passwordVerifier.VerifyAdvance(
+        notNullLongEnoughAndHasLowerCaseLetter
+      );
 
-    expect(result).toBe(false);
-  });
+      expect(result).toBe(true);
+    }, 3100);
 
-  test("password should have one uppercase letter at least", () => {
-    const result = passwordVerifier.Verify("VeryLongPassword1");
+    test("password should pass if at least three conditions are met with mandatory one", async () => {
+      const shouldPass = "vP1125415";
+      const result = await passwordVerifier.VerifyAdvance(shouldPass);
 
-    expect(result).toBe(true);
-
-    const result2 = passwordVerifier.Verify("verylongpasswordallsmallfails1");
-
-    expect(result2).toBe(false);
-  });
-
-  test("password should have one lowercase letter at least", () => {
-    const result = passwordVerifier.Verify("VeryLongPassword1");
-
-    expect(result).toBe(true);
-
-    const result2 = passwordVerifier.Verify("VERY_LONG_ALL_UPPER_FAILS1");
-
-    expect(result2).toBe(false);
-  });
-
-  test("password should have one number at least", () => {
-    const result = passwordVerifier.Verify("VeryLongPassword1");
-
-    expect(result).toBe(true);
-
-    const result2 = passwordVerifier.Verify("VeryLongPassword");
-
-    expect(result2).toBe(false);
+      expect(result).toBe(true);
+    }, 5500);
   });
 });
