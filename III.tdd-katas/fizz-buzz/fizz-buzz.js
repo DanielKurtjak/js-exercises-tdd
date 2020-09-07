5d858ec6b0db0b35488e6d4cf5fb7002caf0449c
Daniel Kurtjak <danielkurtjak@gmail.com>
2020-09-07 21:22:26 +0200

    Fizz buzz refactor

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -1,7 +1,8 @@
+const range = (number) => [...Array(number).keys()].map((n) => n + 1);
+
 module.exports = (number) =>
-  [...Array(number).keys()]
-    .map((n) => {
-      const num = n + 1;
+  range(number)
+    .map((num) => {
       if (num % 5 === 0 && num % 3 === 0) return "FizzBuzz";
       if (num % 3 === 0) return "Fizz";
       if (num % 5 === 0) return "Buzz";
